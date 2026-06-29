cbabfa98323b9b78961e81c816492fdf67e89cb0
SeanTylerLee <seantylerlee@outlook.com>
2026-06-29 09:02:44 -0500

    d

--- js/main.js
+++ js/main.js
@@ -1,10 +1,4 @@
 runWhenReady(() => {
-  const versionEl = document.getElementById('site-version');
-  if (versionEl && window.SITE_VERSION) {
-    versionEl.textContent = window.SITE_VERSION;
-    versionEl.hidden = false;
-  }
-
   initNav('index');
 
   const loginForm = document.getElementById('login-form');
